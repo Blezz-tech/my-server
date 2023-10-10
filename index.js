c6e73d544923aaab122fda1f0393afd239dc766d
blezz-tech <markus.jenya04@yandex.ru>
2023-10-10 11:59:14 +0300

    lesson-6

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express(); // it's working
+const product = ["Apple", "Pen", "Computer"];
 
-app.get('/', (req, res, next) => {
-    res.send("Its working");
+app.get("/", (req, res, next) => {
+  res.send("Its working");
+});
+
+app.get("/products", (req, res, next) => {
+  res.send(product);
 });
 
 app.listen(5000, () => {
-    console.log('Its started', new Date());
+  console.log("Its started", new Date());
 });
