3094d9ac82ac91188b78d3af860a6998422430f3
blezz-tech <markus.jenya04@yandex.ru>
2023-10-10 12:06:50 +0300

    lesson-7

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 
 const app = express(); // it's working
-const product = ["Apple", "Pen", "Computer"];
+const products = ["Apple", "Pen", "Computer"];
 
 app.get("/", (req, res, next) => {
   res.send("Its working");
 });
 
 app.get("/products", (req, res, next) => {
-  res.send(product);
+  res.json({products});
+});
+
+app.get("/products/:id", (req, res, next) => {
+  if (products[req.params.id]) {
+    res.send(products[req.params.id]);
+  } else {
+    res.status(404).send("Product not found");
+  }
 });
 
 app.listen(5000, () => {
