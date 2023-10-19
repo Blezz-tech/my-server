74e2811defd4f4190cd79c0eb680a11e973f398e
blezz-tech <markus.jenya04@yandex.ru>
2023-10-19 10:11:01 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,13 @@ const router = require("./routes/index");
 
 dotenv.config();
 
-
 const app: Express = express();
 const port = process.env.PORT || 5000;
-
 
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
-const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server started on port ${port}`, new Date())
-    );
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-start();
+app.listen(port, () =>
+  console.log(`Server started on port ${port}`, new Date())
+);
