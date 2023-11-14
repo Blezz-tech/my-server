d5fa3a8b4c3efe9152795bdb9c8a3fa8526b149e
blezz-tech <markus.jenya04@yandex.ru>
2023-11-14 09:33:58 +0300

    update

--- flake.nix
+++ flake.nix
@@ -1,29 +1,24 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-  };
+ description = "A simple flake that uses MariaDB";
 
-  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
-    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in {
-      devShells = forEachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [{
-              packages = with pkgs; [
-                mariadb
-              ];
-              enterShell = with pkgs; ''
-              '';
-              # https://devenv.sh/reference/options/
-              services.mysql.package = pkgs.mariadb;
-              services.mysql.enable = true;
-            }];
-          };
-        });
-    };
+ inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+ inputs.devenv.url = "github:cachix/devenv";
+
+ outputs = { self, nixpkgs, devenv, ... } @ inputs:
+   let
+     pkgs = nixpkgs.legacyPackages."x86_64-linux";
+   in
+   {
+     devShell.x86_64-linux = devenv.lib.mkShell {
+       inherit inputs pkgs;
+       modules = [
+         ({ pkgs, config, ... }: {
+           # This is your devenv configuration
+           packages = [ pkgs.mariadb ];
+           services.mariadb.enable = true;
+           services.mariadb.package = pkgs.mariadb;
+         })
+       ];
+     };
+   };
 }
