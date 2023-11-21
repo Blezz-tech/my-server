bb7a1c4cb30dc4f3a71f4d874c012d781cd21dac
blezz-tech <markus.jenya04@yandex.ru>
2023-11-21 20:15:26 +0300

    update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,14 +5,14 @@ const router = Router();
 // Routers
 import { signupRouter } from "./signupRouter";
 import { loginRouter } from "./loginRouter";
-import { roomRouter } from "./roomRouter";
-import { roomsRouter } from "./roomsRouter";
+// import { roomRouter } from "./roomRouter";
+// import { roomsRouter } from "./roomsRouter";
 
 // use Routers
 router.use("/signup", signupRouter);
 router.use("/login", loginRouter);
-router.use("/room", roomRouter);
-router.use("/rooms", roomsRouter);
+// router.use("/room", roomRouter);
+// router.use("/rooms", roomsRouter);
 router.use("/", (req, res) => {
     res.send('Hello World!')
 })
