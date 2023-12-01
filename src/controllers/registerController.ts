ebed3d0d7e67f36c87f3692af19bb71a4a8ddeef
blezz-tech <markus.jenya04@yandex.ru>
2023-12-01 15:54:39 +0300

    update

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -1,17 +1,46 @@
 import { Request, Response } from 'express';
 import { db } from '../config';
-import { Admins } from '../entity/Admins';
+import { Clients } from '../entity/Clients';
 
 class RegisterController {
     async post(req: Request, res: Response) {
-        const { username, password } = req.body;
+        const { fio, email, phone, id_rooms, birth_date } = req.body;
+        const clients_settings = {
+            "fio": fio,
+            "email": email,
+            "phone": phone,
+            "birth_date": birth_date
+        };
 
-        // const new_user = await db.getRepository(Admins).create({ username, password })
-        // const results = await db.getRepository(Admins).save(new_user)
+
+        for (const [key, value] of Object.entries(clients_settings)) {
+            console.log(`${key}: ${value}`);
+            const checking = {};
+            checking[`${key}`] = value;
+
+            const isExist = await db.getRepository(Clients).exist({
+                where: checking
+            });
+
+            if (isExist) {
+                checking[`${key}`] = [value];
+
+                return res.status(401).json({
+                    "message": "The given data was invalid",
+                    "errors": checking,
+                });
+            }
+        }
+
+        clients_settings["id_rooms"] = id_rooms;
+
+        const new_client = await db.getRepository(Clients).create(clients_settings)
+        const results = await db.getRepository(Clients).save(new_client)
+
 
         res.status(200).send({
             data: {
-                message: "lol krk",
+                message: "Created",
             },
         });
 
