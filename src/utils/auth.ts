aba0dd29991015ec9c84afd3d1f293d4ddb68eb7
blezz-tech <markus.jenya04@yandex.ru>
2023-12-14 09:48:53 +0300

    clear

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -30,22 +30,4 @@ const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
     next();
 };
 
-
-// const checkEmpty =
-//     (req: Request, res: Response, next: NextFunction) => {
-//         const output = {
-//             "message": "The given data was invalid.",
-//             "errors": {}
-//         };
-
-//         const result = validationResult(req);
-//         if (!result.isEmpty()) {
-//             result.array().forEach((item) => {
-//                 output.errors[item["path"]] = [`The ${item["path"]} field is required.`];
-//             });
-//             return res.status(401).send(output);
-//         }
-//         next()
-//     };
-
 export { checkAuth }
